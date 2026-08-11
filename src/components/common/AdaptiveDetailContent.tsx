500833cd51a65bcc3c87f33971eab90fcd4568e8
chusday97 <chusday97@gmail.com>
2026-08-11 19:54:33 +0800

    refine: make desktop detail drawer true half-width

--- src/components/common/AdaptiveDetailContent.tsx
+++ src/components/common/AdaptiveDetailContent.tsx
@@ -14,7 +14,7 @@ export function AdaptiveDetailContent({ className, ...props }: AdaptiveDetailCon
         'flex flex-col overflow-hidden border-border bg-white p-0 shadow-[0_20px_60px_rgba(15,23,42,0.2)] duration-200',
         isPhoneLayout
           ? 'bottom-0 left-1/2 top-auto h-[92dvh] max-h-[92dvh] !w-full !max-w-[430px] -translate-x-1/2 translate-y-0 rounded-b-none rounded-t-[28px]'
-          : 'bottom-0 left-auto right-0 top-0 h-[100dvh] max-h-[100dvh] w-[50vw] min-w-[520px] max-w-[760px] translate-x-0 translate-y-0 rounded-none rounded-l-[28px] data-open:zoom-in-100 data-closed:zoom-out-100 data-open:slide-in-from-right-full data-closed:slide-out-to-right-full',
+          : 'bottom-0 left-auto right-0 top-0 h-[100dvh] max-h-[100dvh] w-[50vw] min-w-[520px] max-w-none translate-x-0 translate-y-0 rounded-none rounded-l-[28px] data-open:zoom-in-100 data-closed:zoom-out-100 data-open:slide-in-from-right-full data-closed:slide-out-to-right-full',
         className,
       )}
       {...props}
