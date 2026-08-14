a8f61efc3410ed4198b456ccc02ddfd7ad449f2b
chusday97 <chusday97@gmail.com>
2026-08-14 10:47:05 +0800

    fix: keep care detail drawer visible at tablet widths

--- src/components/common/AdaptiveDetailContent.tsx
+++ src/components/common/AdaptiveDetailContent.tsx
@@ -28,7 +28,7 @@ export function AdaptiveDetailContent({ className, style, ...props }: AdaptiveDe
         'flex flex-col overflow-hidden border-border bg-white p-0 shadow-[0_20px_60px_rgba(15,23,42,0.16)] duration-200',
         isPhoneLayout
           ? 'bottom-0 left-1/2 top-auto h-[92dvh] max-h-[92dvh] !w-full !max-w-[430px] -translate-x-1/2 translate-y-0 rounded-b-none rounded-t-[28px]'
-          : 'bottom-0 left-auto right-0 top-0 h-[100dvh] max-h-[100dvh] min-w-0 translate-x-0 translate-y-0 rounded-none rounded-l-[24px] data-open:zoom-in-100 data-closed:zoom-out-100 data-open:slide-in-from-right-full data-closed:slide-out-to-right-full',
+          : 'bottom-0 left-auto right-0 top-0 h-[100dvh] max-h-[100dvh] min-w-0 translate-x-0 translate-y-0 rounded-none rounded-l-[24px]',
         className,
       )}
       style={surfaceStyle}
