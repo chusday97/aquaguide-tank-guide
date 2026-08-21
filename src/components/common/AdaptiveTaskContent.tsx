68070298e01ffe2547cac444c9cf85df0173d773
chusday97 <chusday97@gmail.com>
2026-08-21 15:06:01 +0800

    refactor(ui): standardize mobile task sheets from bottom

--- src/components/common/AdaptiveTaskContent.tsx
+++ src/components/common/AdaptiveTaskContent.tsx
@@ -10,11 +10,12 @@ export function AdaptiveTaskContent({ className, ...props }: AdaptiveTaskContent
   return (
     <DialogContent
       data-surface="task-flow"
+      data-task-viewport={isPhoneLayout ? 'phone-sheet' : 'desktop-rail'}
       className={cn(
-        'flex flex-col overflow-hidden border-border bg-[#FDFCF8] p-0 shadow-[0_24px_70px_rgba(15,23,42,0.18)] duration-200',
+        'flex min-h-0 flex-col overflow-hidden border-border bg-[#FDFCF8] p-0 shadow-[0_24px_70px_rgba(15,23,42,0.18)] duration-200',
         isPhoneLayout
-          ? 'bottom-0 left-0 right-auto top-0 h-[100dvh] max-h-[100dvh] w-full max-w-[430px] translate-x-0 translate-y-0 rounded-none data-open:zoom-in-100 data-open:slide-in-from-bottom data-closed:zoom-out-100 data-closed:slide-out-to-bottom'
-          : 'bottom-0 left-auto right-0 top-0 h-[100dvh] max-h-[100dvh] w-[min(860px,calc(100vw-76px))] max-w-[860px] translate-x-0 translate-y-0 rounded-none border-y-0 border-r-0 data-open:zoom-in-100 data-open:slide-in-from-right data-closed:zoom-out-100 data-closed:slide-out-to-right',
+          ? 'bottom-0 left-1/2 right-auto top-auto h-[82dvh] max-h-[92dvh] w-full max-w-[430px] -translate-x-1/2 translate-y-0 rounded-b-none rounded-t-[28px] data-open:zoom-in-100 data-open:slide-in-from-bottom data-closed:zoom-out-100 data-closed:slide-out-to-bottom'
+          : 'bottom-0 left-auto right-0 top-0 h-[100dvh] max-h-[100dvh] w-[min(760px,calc(100vw-320px))] max-w-[760px] translate-x-0 translate-y-0 rounded-l-[28px] rounded-r-none border-y-0 border-r-0 data-open:zoom-in-100 data-open:slide-in-from-right data-closed:zoom-out-100 data-closed:slide-out-to-right',
         className,
       )}
       {...props}
